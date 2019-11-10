b14bb56ee01c4eea13471fa00c9776a623eeb59c
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-10 12:44:34 -0800

    UpdateForm taking shape. setup inputs for title, director, and metascore

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UpdateForm = props => {
+    const [movie, setMovie] = useState({
+        title: "", director: "", metascore: "", stars: []
+    });
+
+    const handleChange = e => setMovie({...movie, [e.target.name]: e.target.value});
+
+    // Function that takes in an event 
+    const handleStar = index => e => {
+        setMovie({...movie, stars: movie.stars.map((star, starIndex) => {
+            if(starIndex === index) {
+                return e.target.value;
+            } else { return star }
+            })})
+    }
+
     return (
         <form>
+            <input type="text"
+                   name="title"
+                   placeholder="title"
+                   value={movie.title}
+                   onChange={handleChange}
+            />
+            <input type="text"
+                   name="director"
+                   placeholder="director"
+                   value={movie.director}
+                   onChange={handleChange}
+            />
+            <input type="text"
+                   name="metascore"
+                   placeholder="metascore"
+                   value={movie.metascore}
+                   onChange={handleChange}
+            />
+            {movie.actors.map((starName, index) => {
+                return  <input type="text"
+                               placeholder="star"
+                               value={starName}
+                               onChange={handleStar(index)}
+                />
+            })}
 
         </form>
     )
